cf0fea5ab8c717291f0a4ec0fa018085a8b53cec
Veda2010 <73531815+Veda2010@users.noreply.github.com>
2021-01-11 15:50:52 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,9 @@
+function updateScore()
+{
+    score = score + 1;
+    document.getElementById("score").innerHTML= "Score: " + score;
+}
+function saveScore()
+{
+    localStorage.setItem("score", score);
+}
